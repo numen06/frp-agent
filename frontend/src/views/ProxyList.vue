86dae4353902d5c010261b58d73cff992449c63e
numen06 <numen06@sina.com>
2026-01-16 17:47:55 +0800

    Update ProxyList.vue

--- frontend/src/views/ProxyList.vue
+++ frontend/src/views/ProxyList.vue
@@ -583,13 +583,19 @@ const navigateToGroupManage = (groupName) => {
     return
   }
   
-  router.push({
-    path: '/groups',
-    query: {
-      server_id: currentServerId.value,
-      group: groupName
-    }
-  })
+  // 直接在当前页面过滤该分组，而不是跳转到分组管理页面
+  proxiesStore.setFilters({ group: groupName })
+  proxiesStore.setPagination({ page: 1 })
+  loadData(1)
+  
+  // 可选：也可以跳转到分组管理页面查看分组详情
+  // router.push({
+  //   path: '/groups',
+  //   query: {
+  //     server_id: currentServerId.value,
+  //     group: groupName
+  //   }
+  // })
 }
 </script>
 
